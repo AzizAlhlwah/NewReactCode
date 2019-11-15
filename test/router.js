7495a5ae57dd2c9bd794c26dbe86c897d19247dd
Abdulaziz <abdulaziz@Abdulazizs-iMac.local>
2019-11-15 13:30:51 +0300

    Ruoter

--- test/router.js
+++ test/router.js
@@ -11,4 +11,28 @@ describe('Router', function(){
     app = express();
   })
 
-  
+   describe('.match(method, url, i)', function(){
+    it('should match based on index', function(){
+      router.route('get', '/foo', function(){});
+      router.route('get', '/foob?', function(){});
+      router.route('get', '/bar', function(){});
+
+      var method = 'GET';
+      var url = '/foo?bar=baz';
+
+      var route = router.match(method, url, 0);
+      route.constructor.name.should.equal('Route');
+      route.method.should.equal('get');
+      route.path.should.equal('/foo');
+
+      var route = router.match(method, url, 1);
+      route.path.should.equal('/foob?');
+
+      var route = router.match(method, url, 2);
+      assert(!route);
+
+      url = '/bar';
+      var route = router.match(method, url);
+      route.path.should.equal('/bar');
+    })
+  })
