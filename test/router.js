7965d105c6a8600872ea16cbaf25efe37c7a9b4d
Abdulaziz <abdulaziz@Abdulazizs-iMac.local>
2019-11-16 20:30:49 +0300

    Des

--- test/router.js
+++ test/router.js
@@ -62,16 +62,3 @@ describe('Router', function(){
     })
   })
 
-   describe('.middleware', function(){
-    it('should dispatch', function(done){
-      router.route('get', '/foo', function(req, res){
-        res.send('foo');
-      });
-
-      app.use(router.middleware);
-
-      request(app)
-      .get('/foo')
-      .expect('foo', done);
-    })
-  })
